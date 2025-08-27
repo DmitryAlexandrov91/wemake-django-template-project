16f52e2003d11ec3a5cd41afef2c8d4380326268
Daria S <63650428+capybalrog@users.noreply.github.com>
2025-08-27 16:55:43 +0300

    Add company app (#43)
    * Add company app
    
    * fix: errors
    
    ---------
    
    Co-authored-by: Alexey Potapov <2furius@gmail.com >

--- server/settings/components/common.py
+++ server/settings/components/common.py
@@ -21,6 +21,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 INSTALLED_APPS: tuple[str, ...] = (
     # Your apps go here:
+    'server.apps.company',
     'server.apps.tgbot',
     'server.apps.users',
     'server.apps.surveys',
