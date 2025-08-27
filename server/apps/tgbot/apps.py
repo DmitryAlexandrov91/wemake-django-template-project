78f59af30a86efea54002534c1d9cee64d3708b8
Novev Denis <128604234+m1dden@users.noreply.github.com>
2025-08-27 15:13:22 +0400

    Create tgbot apps and test, issue#40 (#44)
    * Create tgbot apps and test, issue#40
    
    * FixRev-issue-40
    
    ---------
    
    Co-authored-by: m1dden <denis@DESKTOP-91BAQK2>

---
+++ server/apps/tgbot/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class TgbotConfig(AppConfig):
+    """Configuration for the tgbot Django application."""
+
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'server.apps.tgbot'
