1024254e03afcb9223d5ff476be8a6e795660369
Daria S <dk17march@yandex.ru>
2025-08-31 16:45:18 +0300

    Move test tg token to env.

--- server/settings/components/tgbot.py
+++ server/settings/components/tgbot.py
@@ -1,17 +1,3 @@
-import os
-import sys
-
 from server.settings.components import config
 
-is_test_mode = (
-    'pytest' in sys.modules
-    or 'test' in sys.argv
-    or 'test' in os.environ.get('DJANGO_SETTINGS_MODULE', '')
-    or os.environ.get('PYTEST_CURRENT_TEST')
-)
-
-
-if is_test_mode:
-    BOT_TOKEN = '1234567890:AAFakeTokenForTesting123456789'  # noqa: S105
-else:
-    BOT_TOKEN = config('TG_BOT_TOKEN')
+BOT_TOKEN = config('TG_BOT_TOKEN')
