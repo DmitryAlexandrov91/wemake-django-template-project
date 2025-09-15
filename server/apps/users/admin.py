d69351fc084b6f3dc3e160be1aeef2aceca31712
Alexey Potapov <2furius@gmail.com>
2025-09-15 18:40:52 +0300

    fix: change useradmin class and fields (#98)
    Co-authored-by: Alexey Potapov <2furius@gmail.com >

--- server/apps/users/admin.py
+++ server/apps/users/admin.py
@@ -1,15 +1,22 @@
+from typing import Final
+
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from server.apps.users.models import CustomUser
 
+_EMAIL_FIELD: Final = 'email'
+_FULL_NAME_FIELD: Final = 'full_name'
+_FIELDS: Final = 'fields'
+
 
 @admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin[CustomUser]):
-    """Admin-class for CustomUser."""
+class CustomUserAdmin(BaseUserAdmin[CustomUser]):  # type: ignore[type-var]
+    """This class represents user in admin panel."""
 
     list_display = (
-        'email',
-        'full_name',
+        _EMAIL_FIELD,
+        _FULL_NAME_FIELD,
         'is_staff',
         'is_active',
     )
@@ -17,8 +24,50 @@ class CustomUserAdmin(admin.ModelAdmin[CustomUser]):
         'is_staff',
         'is_active',
         'groups',
+        'department',
     )
-    search_fields = ('email', 'full_name')
-    ordering = ('email',)
+    search_fields = (_EMAIL_FIELD, _FULL_NAME_FIELD)
+    ordering = (_EMAIL_FIELD,)
     filter_horizontal = ('groups', 'user_permissions')
     list_select_related = ('department',)
+
+    fieldsets = (
+        (None, {_FIELDS: (_EMAIL_FIELD, 'password')}),
+        (
+            'Personal info',
+            {
+                _FIELDS: (
+                    _FULL_NAME_FIELD,
+                    'position',
+                    'role',
+                    'department',
+                    'tg_id',
+                )
+            },
+        ),
+        (
+            'Permissions',
+            {
+                _FIELDS: (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                ),
+            },
+        ),
+        ('Important dates', {_FIELDS: ('last_login', 'edited_at')}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                _FIELDS: ('email', 'full_name', 'password1', 'password2'),
+            },
+        ),
+    )
+
+    readonly_fields = ('edited_at',)
