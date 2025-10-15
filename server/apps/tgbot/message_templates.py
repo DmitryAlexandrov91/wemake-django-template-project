026815113a1353f629f03fae20d585c134aaeb29
Dmitry Alexandrov <167573863+DmitryAlexandrov91@users.noreply.github.com>
2025-10-15 15:41:34 +0300

    message_templates.py added (#164)
    *just  message_templates.py added to tgbot app root

---
+++ server/apps/tgbot/message_templates.py
@@ -0,0 +1,13 @@
+SURVEY_RESULTS_TEMPLATE = """
+📋 Опрос <b>{survey_title}</b>
+
+{answers}
+"""
+
+ANSWER_TEMPLATE = """
+<b>Вопрос №{question_number}:</b>
+<i>{question_text}</i>
+
+Ваш ответ:
+<i>{answer_text}</i>
+"""
